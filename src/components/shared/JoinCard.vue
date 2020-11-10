e7eb051b50016215eb4a9a7163213aedfdf86118
Renatas? <renatasgal420@gmail.com>
2020-11-10 13:08:18 +0200

    fix join card img

--- src/components/shared/JoinCard.vue
+++ src/components/shared/JoinCard.vue
@@ -3,8 +3,14 @@
     class="flex flex-col items-center py-5 cursor-pointer px-7 group bg-cream-500 hover:bg-white hover:shadow-card rounded-large"
     style="min-height: 407px"
   >
-    <Icon icon="join_board" class="flex-shrink-0 w-40 h-40" />
-    <oh3 class="mt-8 text-2xl text-center" style="max-width: 155px">
+    <img
+      src="/central/illustrations/join-card.png"
+      alt="join card img"
+      width="180"
+      height="180"
+      class="flex-shrink-0"
+    />
+    <oh3 class="mt-3 text-2xl text-center" style="max-width: 155px">
       {{ item.title }}
     </oh3>
     <div class="mt-3 border-t border-black" style="width: 39px"></div>
